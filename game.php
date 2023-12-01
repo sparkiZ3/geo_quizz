1f4ef41b9fcfe80cae3d62270376fa400b527fd1
Noan Lissillour <noan.lissillour@gmail.com>
2023-12-01 14:49:27 +0100

    patch cookies

--- game.php
+++ game.php
@@ -14,6 +14,15 @@
 
 <body>
     <?php 
+    if (isset($_GET['nb'])==1) {
+        setcookie('nb', $_GET['nb'],time() + (86400 * 30),);
+    }
+    if (isset($_GET['skip'])==1) {
+        setcookie('flag', $liste_pays[rand(0, $nb_flags)],time() + (86400 * 30),);
+        setcookie('drp_seen', $_COOKIE['drp_seen']+1,time() + (86400 * 30), "/");
+        header('Location: game.php');
+    }
+
     if (isset($_GET['flaginit'])) {
         setcookie('flag', $_GET['flaginit'],time() + (10000), );
     }
@@ -24,6 +33,8 @@
         if (strtoupper($guess) === $pays[$_COOKIE['flag']]) {
             $correct_guess = true;
             setcookie('flag', $liste_pays[rand(0, $nb_flags)],time() + (86400 * 30),);
+            setcookie('score',$_COOKIE['score']+1,time() + (86400 * 30),);
+            setcookie('drp_seen', $_COOKIE['drp_seen']+1,time() + (86400 * 30), "/");
             header('Location: game.php');
         }elseif (strtoupper($guess) !== strtoupper($pays[$_COOKIE['flag']])) {
             $correct_guess = false;
@@ -35,15 +46,22 @@
         <div class="top_img">
             <div></div>
             <h2>Quel est le nom de ce pays ?</h2>
-            <h3>Bonnes réponses : ../20</h3>
+            <h3>Bonnes réponses : <?php echo $_COOKIE['score']."/".$_COOKIE['nb']?></h3>
         </div>
         
         <img src="flags/<?php echo $_COOKIE['flag'];?>.png">
         <form method="get">
             <input name="guess" id="nom" type="text" />
          </form>
+         <a href="game.php?skip=1"><div class="skip"><h2>je passe >></h2></div></a>
+         <div class="indice">
+            <h2>Indice</h2>
+            <p>code pays :<?php echo $_COOKIE['flag'];?></p>
+         </div>
     </div>
+    
     <?php
+    //echo $_COOKIE['score']
     ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,10 +1,52 @@
-<?php include 'lst_flags.php';?>
-<a href="game.php?flaginit=<?php echo $liste_pays[rand(0, 120)]; ?>"><div></div></a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php 
+    $nb_flags = 120;
+    ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>flag game</title>
+    <link rel="stylesheet" type="text/css" href="style_index.css" />
+</head>
+<body>
+    <?php include 'lst_flags.php';
+    setcookie('score', '0',time() + (86400 * 30), "/");
+    setcookie('drp_seen', '0',time() + (86400 * 30), "/");
+    ?>
 
-<style>
-    div{
-        width: 100px;
-        height: 100px;
-        background-color: red;
-    }
-</style>
+    <a href="game.php?flaginit=<?php echo $liste_pays[rand(0, 120)]; ?>"><div></div></a>
+<fieldset>
+    <legend>nombre de drapeaux a deviner</legend>
+    <form action="game.php" method="get" class="form-example">
+        <div class="checkbox">
+            <div>
+                <input type="radio"name="nb" value="20" required />
+                <label for="huey">20</label>
+            </div>
+            <div>
+                <input type="radio"name="nb" value="50" />
+                <label for="dewey">50</label>
+            </div>
+            <div>
+                <input type="radio"name="nb" value="100" />
+                <label for="dewey">100</label>
+            </div>
+            <div>
+                <input type="radio"name="nb" value="150" />
+                <label for="dewey">150</label>
+            </div>
+            <div>
+                <input type="radio"name="nb" value="200" />
+                <label for="dewey">200</label>
+            </div>
+            <div>
+                <input type="radio" name="nb" value="250" />
+                <label for="louie">250</label>
+            </div>
+        </div>
+        <button type="submit">Jouer !</button>
+    </form>
+</fieldset>
+    
+</body>
